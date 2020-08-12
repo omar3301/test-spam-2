7c74f75e82a97ebfb6784568a1cdd7703780489e
omar3301 <68645745+omar3301@users.noreply.github.com>
2020-08-12 20:24:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,35 +24,16 @@ client12.login(process.env.TOKEN12);
 
 
 client7.on('message', message => {
-  if (message.content === 'd1') {
+  if (message.content === 'd7') {
 message.channel.send('#daily')
   }
-  if(message.content === 'c1'){
+  if(message.content === 'c7'){
 message.channel.send('#credits')
   }
-  if(message.content === 'r1'){
+  if(message.content === 'r7'){
 message.channel.send("#rep "+  443168460030345227 )
   }
-  if (message.content === 'p1') {
-message.channel.send('#profile')
-  }
-  if(message.content.startsWith("s1")) {
-    var text = message.content.split(' ').slice(1).join(' ');
-     message.channel.send(text);
-  }
-});
- 
-client8.on('message', message => {
-  if (message.content === 'd2') {
-message.channel.send('#daily')
-  }
-  if(message.content === 'c2'){
-message.channel.send('#credits')
-  }
-   if(message.content === 'r2'){
-message.channel.send("#rep "+  443168460030345227 )
-  }
-  if (message.content === 'p2') {
+  if (message.content === 'p7') {
 message.channel.send('#profile')
   }
   if(message.content.startsWith("s2")) {
@@ -61,77 +42,96 @@ message.channel.send('#profile')
   }
 });
  
-client9.on('message', message => {
-  if (message.content === 'd3') {
+client8.on('message', message => {
+  if (message.content === 'd8') {
 message.channel.send('#daily')
   }
-  if(message.content === 'c3'){
+  if(message.content === 'c8'){
 message.channel.send('#credits')
   }
-  if(message.content === 'r3'){
+   if(message.content === 'r8'){
 message.channel.send("#rep "+  443168460030345227 )
   }
-  if (message.content === 'p3') {
+  if (message.content === 'p8') {
 message.channel.send('#profile')
   }
-  if(message.content.startsWith("s3")) {
+  if(message.content.startsWith("s8")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+client9.on('message', message => {
+  if (message.content === 'd9') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c9'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r9'){
+message.channel.send("#rep "+  443168460030345227 )
+  }
+  if (message.content === 'p9') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s9")) {
     var text = message.content.split(' ').slice(1).join(' ');
      message.channel.send(text);
   }
 });
  
 client10.on('message', message => {
-  if (message.content === 'd4') {
+  if (message.content === 'd10') {
 message.channel.send('#daily')
   }
-  if(message.content === 'c4'){
+  if(message.content === 'c10'){
 message.channel.send('#credits')
   }
-  if(message.content === 'r4'){
+  if(message.content === 'r10'){
 message.channel.send("#rep "+  443168460030345227 )
   }
-  if (message.content === 'p4') {
+  if (message.content === 'p10') {
 message.channel.send('#profile')
   }
-  if(message.content.startsWith("s4")) {
+  if(message.content.startsWith("s10")) {
     var text = message.content.split(' ').slice(1).join(' ');
      message.channel.send(text);
   }
 });
  
 client11.on('message', message => {
-  if (message.content === 'd5') {
+  if (message.content === 'd11') {
 message.channel.send('#daily')
   }
-  if(message.content === 'c5'){
+  if(message.content === 'c11'){
 message.channel.send('#credits')
   }
-  if(message.content === 'r5'){
+  if(message.content === 'r11'){
 message.channel.send("#rep "+  443168460030345227 )
   }
-  if (message.content === 'p5') {
+  if (message.content === 'p11') {
 message.channel.send('#profile')
   }
-  if(message.content.startsWith("s5")) {
+  if(message.content.startsWith("s11")) {
     var text = message.content.split(' ').slice(1).join(' ');
      message.channel.send(text);
   }
 });
 
 client12.on('message', message => {
-  if (message.content === 'd6') {
+  if (message.content === 'd12') {
 message.channel.send('#daily')
   }
-  if(message.content === 'c6'){
+  if(message.content === 'c12'){
 message.channel.send('#credits')
   }
-  if(message.content === 'r6'){
+  if(message.content === 'r12'){
 message.channel.send("#rep "+  443168460030345227 )
   }
-  if (message.content === 'p6') {
+  if (message.content === 'p12') {
 message.channel.send('#profile')
   }
-  if(message.content.startsWith("s6")) {
+  if(message.content.startsWith("s12")) {
     var text = message.content.split(' ').slice(1).join(' ');
      message.channel.send(text);
   }
@@ -140,131 +140,6 @@ message.channel.send('#profile')
 
 //???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
 
-
-client7.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``1`` ")
-    }
-});
-
-client8.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``2`` ")
-    }
-});
-
-client9.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``3`` ")
-    }
-});
-
-client10.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``4`` ")
-    }
-});
-
-client11.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``5`` ")
-    }
-});
-
-client12.on('message', message => {
-    if(message.content === prefix+'number'){
-        message.channel.send(" My Token Is ``6`` ")
-    }
-});
-
-
-//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-
-client7.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client8.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client9.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client10.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client11.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client12.on('message', message => {
-if (message.content === prefix+'fast') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 30000; x++) {
-        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
 
 client7.on('message', message => {
     if(message.content === prefix+'number'){
@@ -278,26 +153,117 @@ client8.on('message', message => {
     }
 });
 
-client3.on('message', message => {
+client9.on('message', message => {
     if(message.content === prefix+'number'){
         message.channel.send(" My Token Is ``9`` ")
     }
 });
 
-client4.on('message', message => {
+client10.on('message', message => {
     if(message.content === prefix+'number'){
         message.channel.send(" My Token Is ``10`` ")
     }
 });
 
-client5.on('message', message => {
+client11.on('message', message => {
     if(message.content === prefix+'number'){
         message.channel.send(" My Token Is ``11`` ")
     }
 });
 
-client6.on('message', message => {
+client12.on('message', message => {
     if(message.content === prefix+'number'){
         message.channel.send(" My Token Is ``12`` ")
     }
 });
+
+
+//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+
+client7.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client8.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client9.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client10.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client11.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client12.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+//???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+
+
